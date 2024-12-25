3b753209ecc4c70e961984a6629685fb9f1dddeb
Federico Cerutti <fred68to@hotmail.it>
2024-12-25 14:14:38 +0100

    Aggiungere i file di progetto.

---
+++ CfgReader.NET/Form1.cs
@@ -0,0 +1,11 @@
+namespace CfgReader.NET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Usare la versione CfgReader (Console) del programma, come esempio.");
+        }
+    }
+}
---
+++ CfgReader/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using Fred68.CfgReader;
+
+
+string filename = "esempio.txt";
+Console.WriteLine("Avvio programma.");
+
+// UTILIZZO SEMPLICE COME DIZIONARIO
+
+Console.WriteLine("Utilizzo come dizionario...");
+
+dynamic cfgR = new CfgReader();							// Crea l'oggetto della classe base
+cfgR.CHR_ListSeparator = @";";
+cfgR.ReadConfiguration(filename);						// Legge la configurazione		
+Console.WriteLine(cfgR.ToString());						// Visualizza i messaggi
+try
+	{
+	Console.WriteLine("cc = " + cfgR.cc);
+	cfgR.cc = 100.3f;
+	Console.WriteLine("cc = " + cfgR.cc + " riassegnata");
+	}
+catch (Exception ex)
+	{
+	Console.WriteLine(ex.Message);
+	}
+
+#if true
+Console.WriteLine("\nVariabili importate:");			// Comando DUMP
+Console.WriteLine(cfgR.DumpEntries());
+Console.WriteLine("Linee:");							// Comando LINES
+Console.WriteLine(cfgR.DumpLines());
+#endif
+
+
+// UTILIZZO CON CLASSE DERIVATA
+
+Console.WriteLine("Utilizzo con classe derivata...");
+			
+CfgR2 cf2 = new CfgR2();							// Crea l'oggetto della classe derivata
+cf2.CHR_ListSeparator = @";";	
+cf2.ReadConfiguration(filename);					// Legge la configurazione (classe base)
+bool ok = cf2.GetNames(true);						// Imposta le variabili (da classe base a derivata), eliminandole dal dizionario
+Console.WriteLine(cf2.ToString());					// Visualizza i messaggi (da classe base)
+Console.WriteLine(cf2.Dump());						// Stampa le variabili (funzione della classe derivata)
+Console.WriteLine(cf2.DumpEntries());				// Stampa il contenuto del dizionario
+cf2.Clear();										// Cancella tutti i dati letti (della classe base)
+
+Console.WriteLine("Fine programma.");
+Console.ReadKey();
+
+class CfgR2 : CfgReader
+	{
+	public int Paperino;
+	public float ccc;
+	public List<int> ddd;
+
+	public string Dump()
+		{
+		StringBuilder sb = new StringBuilder();
+		sb.AppendLine($"CfgR2.Dump():");
+		sb.AppendLine($"Paperino={Paperino}");
+		sb.AppendLine($"ccc={ccc}");
+		sb.AppendLine($"ddd={ddd}");
+
+		return sb.ToString();
+		}
+	}
+
